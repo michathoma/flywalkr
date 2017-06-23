48510d6181c32ad60332983e132818bc21c14bc5
Michael Thoma <micha.thoma@gmx.de>
2017-06-23 16:06:22 +1200

    add first roxygen commands

--- R/read.esm.R
+++ R/read.esm.R
@@ -1,28 +1,28 @@
+#' Read all esm-csv in a gven directory.
+#'
+#' Will read all esm files in the given directory and concatenate them in single \code{data.frame}. Filenames will be used to keep track of individual flies in different experiments carrying the same within-experiment number.
+#'
+#'
 read_esm <- function(directory, header=TRUE, delimiter = ";", parser = "_"){
 
-##### will read all .csv files within given directory and combine to a new dataframe
-##### IMPORTANT: .csv have to be exported with ; as separator (default in EXCEL, no worries there), decimal separator must be "."
-##### Also IMPORTANT: file names have to be in the format: e.g. 071210a_..., as the first part will be used to rename the flies (in case of multiple experiments)
+  oldwd <- getwd()
+  setwd(directory)
 
-oldwd <- getwd()
-setwd(directory)
+  files <- dir()
 
-files <- dir()
+  file_index<-1:length(files)
+  combdata <- numeric()
 
-file_index<-1:length(files)
-combdata <- numeric()
+  for (i in file_index){
+    file_i <- read.csv(files[i], header==TRUE, sep=delimiter)
+    experiment <- strsplit(files[i],parser)[[1]][1]
+    file_i$fly.nr <- paste(rep(experiment,dim(file_i)[1]),file_i$fly.nr,sep="_")
+    combdata <- rbind(combdata, file_i)
+    print(files[i])
+  }
 
-for (i in file_index)
-{
-file_i <- read.csv(files[i], header==TRUE, sep=delimiter)
-experiment <- strsplit(files[i],parser)[[1]][1]
-file_i$fly.nr <- paste(rep(experiment,dim(file_i)[1]),file_i$fly.nr,sep="_")
-combdata <- rbind(combdata, file_i)
-print(files[i])
-}
+  combdata<-combdata[order(combdata$odour,combdata$fly.nr),]
 
-combdata<-combdata[order(combdata$odour,combdata$fly.nr),]
-
-setwd(oldwd)
-return(combdata)
+  setwd(oldwd)
+  return(combdata)
 }
