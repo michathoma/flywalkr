2440e30b22ff47cd8b55ca43aeccdbfaacc3c610
Michael Thoma <micha.thoma@gmx.de>
2017-06-23 15:56:14 +1200

    compact code; remove header = FALSE option

--- R/read.esm.R
+++ R/read.esm.R
@@ -4,45 +4,25 @@ read_esm <- function(directory, header=TRUE, delimiter = ";", parser = "_"){
 ##### IMPORTANT: .csv have to be exported with ; as separator (default in EXCEL, no worries there), decimal separator must be "."
 ##### Also IMPORTANT: file names have to be in the format: e.g. 071210a_..., as the first part will be used to rename the flies (in case of multiple experiments)
 
-
+oldwd <- getwd()
 setwd(directory)
-nfile<-dir()
-i.lf<-1:length(dir())
+
+files <- dir()
+
+file_index<-1:length(files)
 combdata <- numeric()
 
-for (i in i.lf)
+for (i in file_index)
 {
-if (header==FALSE) {
-pcd <- read.csv(nfile[i],sep=delimiter)
-
-#### renaming starts here, split filename, the part before the _ will be used...
-
-exp<-strsplit(nfile[i],parser)[[1]][1]
-
-#### ...and rename by pasting experiment plus fly nr. from the esm
-
-pcd$fly.nr<-paste(rep(exp,dim(pcd)[1]),pcd$fly.nr)
-
-combdata <- rbind(combdata, pcd)
-print(nfile[i])
-}
-else {
-pcd <- read.csv(nfile[i],header==TRUE,sep=delimiter)
-
-
-exp<-strsplit(nfile[i],parser)[[1]][1]
-pcd$fly.nr<-paste(rep(exp,dim(pcd)[1]),pcd$fly.nr,sep="_")
-
-combdata <- rbind(combdata, pcd)
-print(nfile[i])
-}
+file_i <- read.csv(files[i], header==TRUE, sep=delimiter)
+experiment <- strsplit(files[i],parser)[[1]][1]
+file_i$fly.nr <- paste(rep(experiment,dim(file_i)[1]),file_i$fly.nr,sep="_")
+combdata <- rbind(combdata, file_i)
+print(files[i])
 }
 
 combdata<-combdata[order(combdata$odour,combdata$fly.nr),]
 
-
-#### setwd path has to be changed individually for different users/computers
-
-setwd("C:/Documents and Settings/tretzke/My Documents/")
+setwd(oldwd)
 return(combdata)
 }
