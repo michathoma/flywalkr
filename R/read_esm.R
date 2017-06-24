3f25a7675a734d93be25fde6369d8f27e3da2465
Michael Thoma <micha.thoma@gmx.de>
2017-06-24 05:12:44 +0200

    run roxygen

--- R/read_esm.R
+++ R/read_esm.R
@@ -1,9 +1,16 @@
 #' Read all esm-csv in a gven directory.
 #'
-#' Will read all esm files in the given directory and concatenate them in single \code{data.frame}. Filenames will be used to keep track of individual flies in different experiments carrying the same within-experiment number.
+#' Will read all esm files in the given directory and concatenate them in single
+#' \code{data.frame}. Filenames will be used to keep track of individual flies
+#' in different experiments carrying the same within-experiment number.
 #'
-#'
-read_esm <- function(directory, header=TRUE, delimiter = ";", parser = "_"){
+#' @param directory directory containing extended speed matrizes
+#' @param delimiter delimiter used in extended speed matrizes, defaults to ";"
+#'   (output from German MS Excel)
+#' @param parser pattern to parse file names for naming of individual flies
+#' @return \code{data.frame} containing combined data
+
+read_esm <- function(directory, delimiter = ";", parser = "_"){
 
   oldwd <- getwd()
   setwd(directory)
@@ -14,7 +21,7 @@ read_esm <- function(directory, header=TRUE, delimiter = ";", parser = "_"){
   combdata <- numeric()
 
   for (i in file_index){
-    file_i <- read.csv(files[i], header==TRUE, sep=delimiter)
+    file_i <- read.csv(files[i], header = TRUE, sep = delimiter)
     experiment <- strsplit(files[i],parser)[[1]][1]
     file_i$fly.nr <- paste(rep(experiment,dim(file_i)[1]),file_i$fly.nr,sep="_")
     combdata <- rbind(combdata, file_i)
